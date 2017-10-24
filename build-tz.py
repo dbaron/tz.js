a4b01515bc967f03891a46b64419b7e114f0a1e1
L. David Baron <dbaron@dbaron.org>
2017-10-24 16:09:41 -0700

    Define HAVE_SNPRINTF to fix build issue on some machines with 2017c.

--- build-tz.py
+++ build-tz.py
@@ -45,7 +45,7 @@ def build_tz(destdir, tzcodetar, tzdatatar):
                           cwd=sourcedir)
     subprocess.check_call(["make", "install",
                            "TOPDIR=" + outputdir,
-                           "CFLAGS=-DHAVE_GETTEXT=1 -DTM_GMTOFF=tm_gmtoff -DTM_ZONE=tm_zone"],
+                           "CFLAGS=-DHAVE_GETTEXT=1 -DHAVE_SNPRINTF=1 -DTM_GMTOFF=tm_gmtoff -DTM_ZONE=tm_zone"],
                           cwd=sourcedir)
 
 if __name__ == '__main__':
